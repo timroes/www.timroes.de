14744744620b90939aaca24b0fba6d532102c7aa
Tim Roes <mail@timroes.de>
2016-04-03 13:11:11 +0200

    (build): print related post in chronical order

--- gulp/handlebarHelpers.js
+++ gulp/handlebarHelpers.js
@@ -33,10 +33,26 @@ const useFirst = function (...args) {
 	return undefined;
 };
 
+const eachReverse = function (context) {
+	var options = arguments[arguments.length - 1];
+	var ret = '';
+
+	if (context && context.length > 0) {
+		for (var i = context.length - 1; i >= 0; i--) {
+			ret += options.fn(context[i]);
+		}
+	} else {
+		ret = options.inverse(this);
+	}
+
+	return ret;
+};
+
 export default {
 	date,
 	encode,
 	notequals,
 	'use-first': useFirst,
-	'date-iso': dateIso
+	'date-iso': dateIso,
+	'each-reverse': eachReverse
 };
