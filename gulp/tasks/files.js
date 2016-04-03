0d2537676aa2f75b08f89541c46bbc08fc00de85
Tim Roes <mail@timroes.de>
2016-04-03 13:12:38 +0200

    (build): remove debug output

--- gulp/tasks/files.js
+++ gulp/tasks/files.js
@@ -2,7 +2,6 @@ export default function(gulp, paths, _, watch) {
 
 	gulp.task('files', () => {
 		return gulp.src(paths.content.files, { base: paths.content.base })
-			.pipe(_.debug())
 			.pipe(gulp.dest(`${paths.build}`));
 	});
 
