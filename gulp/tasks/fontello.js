b31e9e957e79305c76843c674041c213249cd988
Tim Roes <mail@timroes.de>
2016-04-07 01:30:03 +0200

    (build): minify fontello css

--- gulp/tasks/fontello.js
+++ gulp/tasks/fontello.js
@@ -1,8 +1,14 @@
 export default function(gulp, paths, _, watch) {
 
 	gulp.task('fontello', () => {
+
+		const css = _.filter(['**/*.css'], { restore: true });
+
 		return gulp.src('./fontello.json')
 			.pipe(_.fontello())
+			.pipe(css)
+			.pipe(_.cleanCss())
+			.pipe(css.restore)
 			.pipe(gulp.dest(`${paths.build}/icons`));
 	});
 
