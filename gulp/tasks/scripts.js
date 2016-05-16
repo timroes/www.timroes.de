1a75610846324e244b9544c339d591a51bdb15e9
Tim Roes <mail@timroes.de>
2016-05-16 19:30:28 +0200

    (build): use webpack uglifyJs plugin to minify code

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,19 +1,22 @@
 import posts from '../data/posts';
-import webpack from 'webpack-stream';
+import webpackStream from 'webpack-stream';
+import webpack from 'webpack';
 import merge from 'merge-stream';
 
 export default function(gulp, paths, _, watch, pipelines) {
 
 	gulp.task('scripts', () => {
 		return gulp.src(paths.sources.scripts)
-			.pipe(webpack({
+			.pipe(webpackStream({
 				module: {
 					loaders: [
 						{ test: /\.js$/, loader: 'babel' }
 					]
-				}
+				},
+				plugins: [
+					new webpack.optimize.UglifyJsPlugin()
+				]
 			}))
-			.pipe(_.uglify())
 			.pipe(_.rename('app.min.js'))
 			.pipe(gulp.dest(paths.build));
 	});
