fef9191b521d82e49f8e76c3c11acc40bbe14ec0
Tim Roes <mail@timroes.de>
2016-05-16 22:14:58 +0200

    (build): remove unused variable in script

--- src/scripts/relatedPosts.js
+++ src/scripts/relatedPosts.js
@@ -3,7 +3,7 @@ var $ = require('jquery');
 $(() => {
 	const linklist = $('.serieslinks-linklist');
 	linklist.css('max-height', linklist.height() + 10);
-	const serieslinks = $('.serieslinks').addClass('expandable');
+	$('.serieslinks').addClass('expandable');
 	$('.serieslinks-label')
 		.click(() => {
 			$('.serieslinks').toggleClass('expanded');
