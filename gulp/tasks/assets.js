56133893f47479373e4066f7abc6dc0e0912b8dd
Tim Roes <mail@timroes.de>
2016-05-29 10:53:43 +0200

    (build): optimize svg assets

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -1,6 +1,10 @@
 export default function(gulp, paths, _, watch) {
 	gulp.task('assets', () => {
+		const svgFilter = _.filter('**/*.svg', { restore: true });
 		return gulp.src(paths.sources.assets, { base: paths.src })
+			.pipe(svgFilter)
+			.pipe(_.svgmin())
+			.pipe(svgFilter.restore)
 			.pipe(gulp.dest(paths.build));
 	});
 
