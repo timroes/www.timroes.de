ea1b8abf0645a2326e04e949f02615610f656909
Tim Roes <mail@timroes.de>
2016-08-27 21:38:08 +0200

    Load all fonts async and repace if loaded fast enough

--- src/scripts/fontLoading.js
+++ src/scripts/fontLoading.js
@@ -1,18 +1,24 @@
-const FontFaceObserver = require('fontfaceobserver');
-const $ = require('jquery');
+const FontFaceObserver = require('fontfaceobserver/fontfaceobserver.js');
 
-const roboto = new FontFaceObserver('Roboto', {
-  weight: 300
+const fonts = [
+	{ family: 'Roboto', weight: 300, style: 'normal' },
+	{ family: 'Roboto', weight: 700, style: 'normal' },
+	{ family: 'Roboto', weight: 300, style: 'italic' },
+	{ family: 'Roboto', weight: 700, style: 'italic' },
+	{ family: 'Source Code Pro', weight: 'normal', style: 'normal' }
+];
+
+console.log("[FONT] Start font loading at %f", window.performance.now())
+const fontPromises = fonts.map(font => {
+	return new FontFaceObserver(font.family, { weight: font.weight, style: font.style }).load(null, 5000);
 });
 
-roboto.load(null, 10000).then(() => {
-  const now = window.performance.now();
-  if (!window.performance || window.performance.now() < 1500) {
-    console.log("Replace font!");
-    $(document.body).addClass('font-loaded');
-  } else {
-    console.debug("Font loaded, but too late.", window.performance.now() / 1000);
-  }
-}, () => {
-  console.log("Failed to load Roboto.", arguments);
-});
+Promise.all(fontPromises).then(() => {
+	const now = window.performance.now();
+	if (!window.performance || window.performance.now() < 1500) {
+		document.body.classList.add('font-loaded');
+		console.log("[FONT] Replace font after %f.", window.performance.now());
+	} else {
+		console.log("[FONT] Font loaded, but too late at %f.", window.performance.now());
+	}
+})
