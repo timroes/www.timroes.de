609cc3c04e5de933f8ab9c2820ebbc6135c081a5
Tim Roes <mail@timroes.de>
2016-08-27 21:12:39 +0200

    Use compression instead of connect-gzip
    Also remove --gzip flag, since compression doesn't seem to fail all the time.

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,16 +1,12 @@
-import gzip from 'connect-gzip';
+import compression from 'compression';
 
 export default function(gulp, paths, _) {
 
 	gulp.task('serve', () => {
-		const middlewares = [];
-		if (_.util.env.gzip !== undefined) {
-			middlewares.push(gzip.gzip());
-		}
 		_.connect.server({
 			root: paths.build,
 			livereload: true,
-			middleware: () => middlewares
+			middleware: () => [ compression() ]
 		});
 	});
 
