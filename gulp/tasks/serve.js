690845f5e3ee9dac05fdccad17f83b4b58527a47
Tim Roes <mail@timroes.de>
2016-09-04 01:30:37 +0200

    Add more debugging options to dev server
    Add the --no-main-css and --delay-main-css option to check how well ATF CSS works.

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,12 +1,45 @@
 import compression from 'compression';
+import slow from 'connect-slow';
+import cache from 'cache-control';
 
 export default function(gulp, paths, _) {
+
+	function failFile(filename) {
+		return (req, res, next) => {
+			if (req.url.startsWith(filename)) {
+				_.util.log(_.util.colors.yellow(`Returned error 500 for ${filename}`));
+				req.statusCode = 500;
+				res.end();
+			} else {
+				next();
+			}
+		};
+	}
+
+	function delayFile(filename) {
+		return slow({
+			url: new RegExp(`^${filename}`),
+			delay: 5000
+		});
+	}
+
+	const middlewares = [
+		compression(), // Always add gzip compression
+		cache({ '/**': 500 })
+	];
+
+	if (_.util.env['delay-main-css'] !== undefined) {
+		middlewares.push(delayFile('/main.min.css'));
+	} else if (_.util.env['main-css'] === false) {
+		// Disable delivery of main css (only above the fold css will be delivered)
+		middlewares.push(failFile('/main.min.css'));
+	}
 
 	gulp.task('serve', () => {
 		_.connect.server({
 			root: paths.build,
 			livereload: true,
-			middleware: () => [ compression() ]
+			middleware: () => middlewares
 		});
 	});
 
