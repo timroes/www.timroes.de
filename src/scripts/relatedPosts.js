e6ce344cfb213e9226f664e98a9c6e1defaa4e15
Tim Roes <mail@timroes.de>
2016-09-04 12:49:10 +0200

    Fix recalculation of related posts container height after CSS load

--- src/scripts/relatedPosts.js
+++ src/scripts/relatedPosts.js
@@ -1,11 +1,19 @@
 var $ = require('jquery');
 
-$(() => {
+function recalculateMaxHeight() {
 	const linklist = $('.serieslinks-linklist');
-	linklist.css('max-height', linklist.height() + 10);
-	$('.serieslinks').addClass('expandable');
+	const serieslinks = $('.serieslinks');
+	linklist.css('max-height', 'none');
+	serieslinks.removeClass('expandable');
+	const height = linklist.height() + 10;
+	console.log("[SERIES] Set related posts height to %d", height);
+	linklist.css('max-height', height);
+	serieslinks.addClass('expandable');
 	$('.serieslinks-label')
-		.click(() => {
-			$('.serieslinks').toggleClass('expanded');
-		});
-});
+	.click(() => {
+		$('.serieslinks').toggleClass('expanded');
+	});
+}
+
+$(recalculateMaxHeight);
+$(window).on('css:onload', recalculateMaxHeight);
