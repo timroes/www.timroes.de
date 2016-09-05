13fbe179dc3a81f3c45c964a45ea66c1c5d55647
Tim Roes <mail@timroes.de>
2016-09-05 21:56:26 +0200

    Only execute related posts logic if there are related posts

--- src/scripts/relatedPosts.js
+++ src/scripts/relatedPosts.js
@@ -2,17 +2,19 @@ var $ = require('jquery');
 
 function recalculateMaxHeight() {
 	const linklist = $('.serieslinks-linklist');
-	const serieslinks = $('.serieslinks');
-	linklist.css('max-height', 'none');
-	serieslinks.removeClass('expandable');
-	const height = linklist.height() + 10;
-	console.log("[SERIES] Set related posts height to %d", height);
-	linklist.css('max-height', height);
-	serieslinks.addClass('expandable');
-	$('.serieslinks-label')
-	.click(() => {
-		$('.serieslinks').toggleClass('expanded');
-	});
+	if (linklist.length > 0) {
+		const serieslinks = $('.serieslinks');
+		linklist.css('max-height', 'none');
+		serieslinks.removeClass('expandable');
+		const height = linklist.height() + 10;
+		console.log("[SERIES] Set related posts height to %d", height);
+		linklist.css('max-height', height);
+		serieslinks.addClass('expandable');
+		$('.serieslinks-label')
+		.click(() => {
+			$('.serieslinks').toggleClass('expanded');
+		});
+	}
 }
 
 $(recalculateMaxHeight);
