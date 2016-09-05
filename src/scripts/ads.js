b512d011169a19b719fed5d6d70148ac5d73fefb
Tim Roes <mail@timroes.de>
2016-09-05 21:55:51 +0200

    Only load disqus and adsense if required

--- src/scripts/ads.js
+++ src/scripts/ads.js
@@ -1,8 +1,14 @@
-var d=document, g=d.createElement('script');
-g.type='text/javascript';
-g.async=true;
-g.defer=true;
-g.src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
-document.body.appendChild(g);
+import $ from 'jquery';
 
-(window.adsbygoogle = window.adsbygoogle || []).push({});
+$(() => {
+	if (document.querySelector('.adsbygoogle')) {
+		var d=document, g=d.createElement('script');
+		g.type='text/javascript';
+		g.async=true;
+		g.defer=true;
+		g.src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+		document.body.appendChild(g);
+
+		(window.adsbygoogle = window.adsbygoogle || []).push({});
+	}
+});
--- src/scripts/disqus.js
+++ src/scripts/disqus.js
@@ -1,13 +1,17 @@
-(function() {
-	var d = document, s = d.createElement('script');
-	s.src = '//timroes.disqus.com/embed.js';
-	s.async = true;
-	s.setAttribute('data-timestamp', +new Date());
-	d.head.appendChild(s);
+import $ from 'jquery';
 
-	s = d.createElement('script');
-	s.src = '//timroes.disqus.com/count.js';
-	s.id = 'dsq-count-scr';
-	s.async = true;
-	d.head.appendChild(s);
-})();
+$(() => {
+	if ($('a[name="comments"]').length > 0) {
+		var d = document, s = d.createElement('script');
+		s.src = '//timroes.disqus.com/embed.js';
+		s.async = true;
+		s.setAttribute('data-timestamp', +new Date());
+		d.head.appendChild(s);
+
+		s = d.createElement('script');
+		s.src = '//timroes.disqus.com/count.js';
+		s.id = 'dsq-count-scr';
+		s.async = true;
+		d.head.appendChild(s);
+	}
+});
