3ea120f84fd5f75f3c82ed9ad29ea0eca1746649
Tim Roes <mail@timroes.de>
2016-09-23 00:29:00 +0200

    Create working service worker only in production

--- gulp/tasks/serviceWorker.js
+++ gulp/tasks/serviceWorker.js
@@ -3,6 +3,10 @@ import swprecache from 'sw-precache';
 
 export default function(gulp, paths, _) {
 	gulp.task('serviceworker', ['resources', 'posts', 'pages', 'index'], (done) => {
+		const handleFetch = _.util.env.production !== undefined || _.util.env['with-sw'] !== undefined;
+		if (!handleFetch) {
+			_.util.log(_.util.colors.yellow('Building a service worker that is not actually handling fetch events!'));
+		}
 		swprecache.generate({
 			staticFileGlobs: [
 				`${paths.build}/*.min.js`,
@@ -14,6 +18,7 @@ export default function(gulp, paths, _) {
 				`${paths.build}/manifest.json`
 			],
 			stripPrefix: `${paths.build}/`,
+			logger: _.util.log,
 			// Ignore all request params, since all resources have a cache request param
 			// and we don't use request params anywhere to actually serve different files.
 			ignoreUrlParametersMatching: [/./],
@@ -21,7 +26,8 @@ export default function(gulp, paths, _) {
 				{ urlPattern: /^chrome-extension/, handler: 'networkOnly' }, // ignore chrome-extensions
 				{ urlPattern: /.*/, handler: 'networkFirst' }
 			],
-			verbose: true
+			verbose: true,
+			handleFetch: handleFetch
 		}).then(swstring => {
 			fromString('sw.js', swstring)
 				.pipe(_.uglify())
