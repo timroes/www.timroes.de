1e2e13de61bfecf41c946531d046cb497c2cd07a
Tim Roes <mail@timroes.de>
2016-09-29 21:48:49 +0200

    Improve loading of front page avatar
    Add a pixelated version of the actual image during loading instead of the favicon for the sake of @flash1293 eyes.

--- src/scripts/img-errors.js
+++ src/scripts/img-errors.js
@@ -6,5 +6,8 @@ $(() => {
 		// and remove the image from the DOM.
 		$(this).parent('.image-wrapper').addClass('failed');
 		$(this).remove();
+	}).on('load', function() {
+		// If image loaded successfully add 'ok' class to it
+		$(this).addClass('ok');
 	});
 });
