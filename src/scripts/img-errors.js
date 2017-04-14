14bf0fd254f77237773fdd641d9fada31503ac03
Tim Roes <mail@timroes.de>
2017-04-14 20:37:06 +0200

    Fix small accessibility issue with failed imgs
    Prevent failed images to be removed completely from a11y-tree,
    by giving its wrapper the role=img and the correct label.

--- src/scripts/img-errors.js
+++ src/scripts/img-errors.js
@@ -2,10 +2,17 @@ const $ = require('jquery');
 
 $(() => {
 	$('img').on('error', function(ev) {
+		const img = $(this);
 		// If an image failed loading set the failed class to the image-wrapper
 		// and remove the image from the DOM.
-		$(this).parent('.image-wrapper').addClass('failed');
-		$(this).remove();
+		img.parent('.image-wrapper').addClass('failed')
+			// If the image loading failed and we are about to remove the image from DOM
+			// give the wrapper also an img role and copy the aria-labelledby attribute
+			// from the image to it, so the screenreader will still behave like the
+			// image would be in DOM.
+			.attr('aria-labelledby', img.attr('aria-labelledby'))
+			.attr('role', 'img');
+		img.remove();
 	}).on('load', function() {
 		// If image loaded successfully add 'ok' class to it
 		$(this).addClass('ok');
