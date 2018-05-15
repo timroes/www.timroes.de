ceb3ea6dd9abecea571ca66105ef22e6ba756b98
Tim Roes <mail@timroes.de>
2018-05-15 23:52:22 +0200

    Fix travisci file

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 
 cd build/
 find . -type f -printf '%p' -exec curl -s --write-out " - Status: %{response_code} Size: %{size_upload} Time: %{time_total}s\n" --user $ftpuser:$ftppassword --ftp-create-dirs -T {} ftp://$ftphost/{} \;
+curl -XPOST -H "AccessKey: $bunnycdn_api_key" -H "Content-Type: application/json" https://bunnycdn.com/api/pullzone/$bunnycdn_zone_id/purgeCache -d ""
